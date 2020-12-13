2799585832712cf6f454ff02e95bd7d2e6a280fb
Floyd Kretschmar <floyd.kretschmar@student.uni-tuebingen.de>
2020-12-13 12:44:01 +0100

    Added logic for posts-service

---
+++ posts/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { v4 } from "uuid";
+import bodyParser from "body-parser";
+
+// set up bodyparser for request body
+const app = express();
+app.use(bodyParser.json());
+
+// In memory storage
+const posts = {};
+
+// Get all posts
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+// Add a new post
+app.post("/posts", (req, res) => {
+  const id = v4();
+  const { title } = req.body;
+
+  posts[id] = { id, title };
+
+  res.status(201).send(posts[id]);
+});
+
+// Listen for requests
+app.listen(4000, () => {
+  console.log("Listening on Port 4000.");
+});
