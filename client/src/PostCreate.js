84419ca86b32baa495652922ef39917b14ba7364
Floyd Kretschmar <floyd.kretschmar@student.uni-tuebingen.de>
2020-12-13 17:25:13 +0100

    add initial client app

---
+++ client/src/PostCreate.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function PostCreate() {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post("http://localhost:4000/posts", { title });
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label for="post">Title</label>
+          <input
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            type="text"
+            id="post"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
