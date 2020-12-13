623a218612b6a2e96278822fae86e395bb47f195
Floyd Kretschmar <floyd.kretschmar@student.uni-tuebingen.de>
2020-12-13 18:00:24 +0100

    add commentcreate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
-export default () => {
+const App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr></hr>
+      <PostList></PostList>
     </div>
   );
 };
+
+export default App;
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function PostCreate() {
+const PostCreate = () => {
   const [title, setTitle] = useState("");
 
   const onSubmit = async (event) => {
@@ -23,8 +23,10 @@ export default function PostCreate() {
             className="form-control"
           />
         </div>
-        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-primary mt-2">Submit</button>
       </form>
     </div>
   );
-}
+};
+
+export default PostCreate;
